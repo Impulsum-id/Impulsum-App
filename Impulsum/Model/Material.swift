a8c211d7aecf44501d30285e84ded1d8bcad93b6
Robert Theo <robertheo.rt@gmail.com>
2024-10-15 13:49:04 +0700

    feat: setup local material

--- Impulsum/Model/Material.swift
+++ Impulsum/Model/Material.swift
@@ -8,32 +8,32 @@
 import Foundation
 import CloudKit
 
-internal struct Material: CloudKitableProtocol {
-    let id = UUID()
-    let name: String
-    let imageURL: URL?
-    let record: CKRecord
-
-    // This initializer should convert a CKRecord into a Material object
-    init?(record: CKRecord) {
-        // Ensure the record contains the needed fields
-        guard let name = record["name"] as? String else { return nil }
-        self.name = name
-        let imageAsset = record["image"] as? CKAsset
-        self.imageURL = imageAsset?.fileURL
-        self.record = record
-    }
-    
-    init?(name: String, imageURL: URL?) {
-        let record = CKRecord(recordType: "Materials")
-        record["name"] = name
-        if let url = imageURL {
-            let asset = CKAsset(fileURL: url)
-            record["image"] = asset
-        }
-    
-        self.init(record: record)
-    }
+//internal struct Material: CloudKitableProtocol {
+//    let id = UUID()
+//    let name: String
+//    let imageURL: URL?
+//    let record: CKRecord
+//
+//    // This initializer should convert a CKRecord into a Material object
+//    init?(record: CKRecord) {
+//        // Ensure the record contains the needed fields
+//        guard let name = record["name"] as? String else { return nil }
+//        self.name = name
+//        let imageAsset = record["image"] as? CKAsset
+//        self.imageURL = imageAsset?.fileURL
+//        self.record = record
+//    }
+//    
+//    init?(name: String, imageURL: URL?) {
+//        let record = CKRecord(recordType: "Materials")
+//        record["name"] = name
+//        if let url = imageURL {
+//            let asset = CKAsset(fileURL: url)
+//            record["image"] = asset
+//        }
+//    
+//        self.init(record: record)
+//    }
 
 
     // This computed property should return the CKRecord representation of the Material
@@ -46,4 +46,17 @@ internal struct Material: CloudKitableProtocol {
 //        record["name"] = name as CKRecordValue
 //        return record
 //    }
+//}
+
+struct Material: Codable {
+//    var id = UUID()
+    let name: String
+    let brand: String
+    let color: String
+    let size: String
+    let image: String
+}
+
+struct MaterialsResponse: Codable {
+    let materials: [Material]
 }
