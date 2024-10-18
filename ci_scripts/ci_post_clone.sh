52c31a5da527a9590d8aad9c5587adb0e1f13019
Robert Theo <robertheo.rt@gmail.com>
2024-10-18 22:42:57 +0700

    Fix: ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -5,18 +5,14 @@ if ! command -v xcodegen &> /dev/null; then
     echo "XcodeGen not found. Installing..."
     brew install xcodegen
 fi
-
 # List the contents of the current directory
 ls .
-
 # Change to the project directory (assuming one level up is correct)
 cd ..
-
 # ALL STEPS AFTER CLONE PROJECT
 # Generate the Xcode project using XcodeGen
 echo "Generating Xcode project..."
 xcodegen generate
-
 # Check if the Xcode project (.xcodeproj) was generated
 PROJECT_NAME=$(ls | grep .xcodeproj | head -n 1)
 if [ -z "$PROJECT_NAME" ]; then
@@ -35,16 +31,13 @@ if [ ! -d "$PROJECT_NAME/project.xcworkspace/xcshareddata" ]; then
 fi
 
 ls "$PROJECT_NAME/project.xcworkspace/xcshareddata"
-
 # Check if the swiftpm directory exists and create it if not
 echo "Checking swiftpm..."
 if [ ! -d "$PROJECT_NAME/project.xcworkspace/xcshareddata/swiftpm" ]; then
     echo "swiftpm directory does not exist. Creating..."
     mkdir -p "$PROJECT_NAME/project.xcworkspace/xcshareddata/swiftpm"
 fi
-
 ls "$PROJECT_NAME/project.xcworkspace/xcshareddata/swiftpm"
-
 # Check if Package.resolved exists, if not, create it
 echo "Checking Package.resolved..."
 PACKAGE_RESOLVED_PATH="$PROJECT_NAME/project.xcworkspace/xcshareddata/swiftpm/Package.resolved"
@@ -52,27 +45,35 @@ if [ ! -f "$PACKAGE_RESOLVED_PATH" ]; then
     echo "Package.resolved does not exist. Creating..."
     cat <<EOL > "$PACKAGE_RESOLVED_PATH"
 {
-  "originHash" : "",
+  "originHash" : "2fe68c10a6992be7dad78f36343af8cf90e2fd2f802a686b5a55fc345997b70f",
   "pins" : [
     {
-      "identity" : "",
-      "kind" : "",
-      "location" : "",
+      "identity" : "focusentity",
+      "kind" : "remoteSourceControl",
+      "location" : "https://github.com/rmRizki/FocusEntity",
       "state" : {
-        "revision" : "",
-        "version" : ""
+        "revision" : "5032a1e718b7b01b114f9e1991c772b5d1efd54b",
+        "version" : "1.0.1"
+      }
+    },
+    {
+      "identity" : "realitygeometries",
+      "kind" : "remoteSourceControl",
+      "location" : "https://github.com/maxxfrazer/RealityGeometries",
+      "state" : {
+        "revision" : "e6eb54c7e9a41f2d63b5fd82300cfe4e578e1ac8",
+        "version" : "1.1.2"
       }
     }
   ],
   "version" : 3
 }
+
 EOL
 fi
-
 # Resolve package dependencies to generate Package.resolved
 echo "Resolving package dependencies..."
 xcodebuild -resolvePackageDependencies -project "$PROJECT_NAME" -scheme Impulsum
-
 # Check if Package.resolved was successfully created
 if [ -f "$PACKAGE_RESOLVED_PATH" ]; then
     echo "Package.resolved generated successfully."
